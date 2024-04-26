2d0b0282c1f9bc8ec0b5f2e23fec64d718ecf1fb
hlothaire <lothaire.holcvart78@gmail.com>
2024-04-26 22:39:48 +0200

    added fallbackMethod

--- order-service/src/main/java/com/microservices/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/microservices/orderservice/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.microservices.orderservice.controller;
 
 import com.microservices.orderservice.dto.OrderRequest;
 import com.microservices.orderservice.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -16,8 +17,13 @@ public class OrderController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
     public String placeOrder(@RequestBody OrderRequest orderRequest){
         orderService.placeOrder(orderRequest);
         return "Order Placed Successfully";
+    }
+
+    public String fallbackMethod(OrderRequest orderRequest, RuntimeException runtimeException) {
+        return "Something went wrong, please order later";
     }
 }
